e7ff51c9a29832c798f7447ff6f96cf5413e5768
nathanegvaldez <nathanegvaldez@gmail.com>
2026-04-26 21:32:49 +0800

    WITH skills_demanded AS ( SELECT     * FROM     job_postings_fact     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id WHERE     job_title_short = 'Data Analyst' AND job_location = 'Anywhere' )
    SELECT
        skills_demanded.skills,
        COUNT(*) AS skills_in_demand
    FROM
        skills_demanded
    GROUP BY skills
    ORDER BY skills_in_demand DESC
    LIMIT 5

--- project_sql/3_most_in_demand_skills.sql
+++ project_sql/3_most_in_demand_skills.sql
@@ -5,10 +5,23 @@ What are the most in demand skills for data analysis?
 - focus on all job postings
 - do this to provide insight into most valuable skills for job seekers
 */
+
+WITH skills_demanded AS (
 SELECT
     *
 FROM
     job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-LIMIT 100;
+WHERE
+    job_title_short = 'Data Analyst' AND job_location = 'Anywhere'
+)
+
+SELECT
+    skills_demanded.skills,
+    COUNT(*) AS skills_in_demand
+FROM
+    skills_demanded
+GROUP BY skills
+ORDER BY skills_in_demand DESC
+LIMIT 5 
